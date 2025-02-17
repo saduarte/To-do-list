fb959cdc2ebc90179ba01e1aed4d5e5718af23d1
Sabrina Duarte <sabsduarte@outlook.com>
2025-02-17 16:52:11 -0300

    commit testes automatizados

--- src/main/java/com/duarte/desafio_todolist/entity/Todo.java
+++ src/main/java/com/duarte/desafio_todolist/entity/Todo.java
@@ -1,18 +1,36 @@
 package com.duarte.desafio_todolist.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "todos")
-@Data
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String descricao;
+
     private Boolean realizado;
+
     private int prioridade;
+
+    public Todo(String nome, String descricao, Boolean realizado, int prioridade) {
+
+        if (prioridade < 1 || prioridade > 3) {
+            throw new IllegalArgumentException("Prioridade deve estar entre 1 e 2");
+        }
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
 }
--- src/test/java/com/duarte/desafio_todolist/DesafioTodolistApplicationTests.java
+++ src/test/java/com/duarte/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,44 @@
 package com.duarte.desafio_todolist;
 
+import com.duarte.desafio_todolist.entity.Todo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
 
+	private Todo todo;
+
+	@BeforeEach
+	void setUp() {
+		todo = new Todo("Primeira tarefa", "Descrição da primeira tarefa", false, 1);
+	}
+
+	@AfterEach
+	void tearDown() {
+		todo = null; // Limpa o objeto após cada teste
+	}
+
 	@Test
-	void contextLoads() {
+	void testCreatedTodoSuccess() {
+		Assertions.assertNotNull(todo);
+		Assertions.assertEquals("Primeira tarefa", todo.getNome());
+		Assertions.assertEquals("Descrição da primeira tarefa", todo.getDescricao());
+		Assertions.assertFalse(todo.getRealizado());
+		Assertions.assertEquals(1, todo.getPrioridade());
+	}
+
+	@Test
+	void testCreatedTodoFailure() {
+		assertThrows(IllegalArgumentException.class, () -> {
+				new Todo("", "sem descricao", false, -1);
+		});
 	}
 
 }
