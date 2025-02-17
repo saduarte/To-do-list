c021677ebc17f55127e99d7ff5c03d16f43411c0
Sabrina Duarte <sabsduarte@outlook.com>
2025-02-17 11:30:53 -0300

    primeiro commit

---
+++ src/test/java/com/duarte/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
